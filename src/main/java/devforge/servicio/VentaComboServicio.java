b49e419bfc1a4fd8483bc1469435e9897ccd393b
Gabriel Vicent <rodriguezisaac876@gmail.com>
2025-06-07 16:57:37 -0400

    gurdad

--- src/main/java/devforge/servicio/VentaComboServicio.java
+++ src/main/java/devforge/servicio/VentaComboServicio.java
@@ -3,7 +3,7 @@ package devforge.servicio;
 import devforge.model.Combo;
 import java.util.List;
 import java.util.Map;
-
+ 
 public interface VentaComboServicio {
     void registrarVenta(List<Map<String, Object>> items);
     void descontarStockCombo(Combo combo, int cantidad);
